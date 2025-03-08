0f1bd69b35d2879991245c12ff8b84bc55d64520
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-03-08 08:43:35 +0100

    single card component defined

---
+++ app/singleCard/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SingleCard() {
+  return (
+    <div>
+        <h2>Word</h2>
+        <div>Translation</div>
+        <div>
+            <p>An example sentence.</p>
+        </div>
+    </div>
+  )
+}
+
+export default SingleCard
