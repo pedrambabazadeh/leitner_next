84d507c3b300477a190b6e76e3dc5fda2981f0ac
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-08-16 22:44:20 +0200

    AskAI section added

---
+++ sections/home/ask-AI/AskAI.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Section } from '@/utils'
+
+const AskAI = () => {
+  return (
+    <Section className='px-8 py-16 bg-gray-900'>
+      
+    </Section>
+  )
+}
+
+export default AskAI
--- sections/home/index.js
+++ sections/home/index.js
@@ -2,3 +2,4 @@ export { default as Header } from './header/Header';
 export { default as HowWorks } from './how-works/HowWorks';
 export { default as Features } from './features/Features';
 export { default as ReviewCards } from './review-cards/ReviewCards';
+export { default as AskAI } from './ask-AI/AskAI';
