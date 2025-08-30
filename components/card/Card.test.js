b2903634b807bb778d548326623a910da890f5e8
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-08-30 11:27:22 +0200

    formik implemented on the sign in form

--- components/card/Card.test.js
+++ components/card/Card.test.js
@@ -3,7 +3,7 @@ import {default as Card} from './Card';
 
 describe('Card', () => {
   it('renders the card component', () => {
-    render(<Card>{'hello'}</Card>);
+    render(<Card>hello</Card>);
     expect(screen.getByText('hello')).toBeInTheDocument();
   });
 });
