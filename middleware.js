be5cd3dc2065553336fb07b9d79e39bbbe7130d3
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2025-09-24 19:10:34 +0200

    user dashboard created, middleware to redirections created

---
+++ middleware.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { backendURL } from "./config/backend";
+
+export async function middleware(request) {
+  const {pathname} = request.nextUrl;
+  if (pathname.startsWith('/user') || pathname.startsWith('/dashboard')) {
+    const response = await fetch(`${backendURL}/me`, {
+      method: 'GET',
+      headers: {
+        Cookie: request.headers.get('cookie') || '',
+      }
+    });
+    if (!response.ok) {
+      const loginURL = new URL('/log-in', request.url);
+        return NextResponse.redirect(loginURL);
+    }
+    return NextResponse.next(); //.nextht() allows the request to proceed
+  }
+ 
+}
+
+ export const config = { matcher: ['/user/:path*', '/dashboard/:path*'] }
+ 
+
+ 
