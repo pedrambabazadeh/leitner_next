9b2a8ee7e5c4e7f4720ec075850323baeace612e
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2026-02-02 20:39:59 +0100

    single word frontend improved

--- app/single-word/[word]/page.jsx
+++ app/single-word/[word]/page.jsx
@@ -1,10 +1,20 @@
+"use client"
+
 import React from 'react'
 import { useParams } from 'next/navigation'
+import { Section } from '@/utils';
 
 const page = () => {
-    const word = useParams();
+    const {word} = useParams();
   return (
-    <div>page</div>
+    <Section className='py-20 px-8 mx-auto'>
+      <div className="card">
+        <div className="slider"></div>
+        <div className="details">
+          <h2 className="text-[var(--primary-color)]">sample word {word}</h2>
+        </div>
+      </div>
+    </Section>
   )
 }
 
