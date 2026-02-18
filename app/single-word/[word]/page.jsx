21b8d39cae852389fd83d1cd76782d241fbce059
PEDRAM BABAZADEH <68613205+pedrambabazadeh@users.noreply.github.com>
2026-02-18 19:45:33 +0100

    single word page improved

--- app/single-word/[word]/page.jsx
+++ app/single-word/[word]/page.jsx
@@ -8,10 +8,25 @@ const page = () => {
     const {word} = useParams();
   return (
     <Section className='py-20 px-8 mx-auto'>
-      <div className="card">
-        <div className="slider"></div>
+      <div className="card flex rounded-2xl p-6">
+        <div className="slider w-[50%]"></div>
         <div className="details">
-          <h2 className="text-[var(--primary-color)]">sample word {word}</h2>
+          <h2 className="text-[var(--color-primary)] text-4xl font-bold mb-[8px]">{word}</h2>
+          <p className="light-text mb-[8px] text-lg mb-[12px]"> ipsum</p>
+          <p className='text-[var(--color-accent)] text-xl font-bold mb-[8px]'> Example:</p>
+          <p className="light-text mb-[8px] text-lg mb-[12px]"> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+          <p className='text-[var(--color-accent)] text-xl font-bold mb-[8px]'> usage type:</p>
+          <p className="light-text mb-[8px] text-lg mb-[12px]"> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+          <p className='text-[var(--color-accent)] text-xl font-bold mb-[8px]'> nominal form:</p>
+          <p className="light-text mb-[8px] text-lg mb-[12px]"> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+          <p className='text-[var(--color-accent)] text-xl font-bold mb-[8px]'> prepositions:</p>
+          <p className="light-text mb-[8px] text-lg mb-[12px]"> Lorem, ipsum, dolor </p>
+          <p className='text-[var(--color-accent)] text-xl font-bold mb-[8px]'> Synonyms:</p>
+          <div className="synonyms flex gap-1">
+            <a href="" className='btn-primary-empty font-semibold px-2 py-1 text-sm rounded-lg hover:scale-105 transition'>word</a>
+            <a href="" className='btn-primary-empty font-semibold px-2 py-1 text-sm rounded-lg hover:scale-105 transition'>word</a>
+          </div>
+
         </div>
       </div>
     </Section>
